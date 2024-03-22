aac2fb5471eb9e175c0061f9ed1f507ec8e1f065
Hanus á Tjaldrafløtti <hanusatv@gmail.com>
2024-03-22 02:25:16 +0100

    final

--- example/Models/Author.cs
+++ example/Models/Author.cs
@@ -1,20 +1,29 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Example.Models
 {
     public class Author
     {
-        public Author() {}
+        public Author()
+        {
+            // Generate a new unique identifier for this Author instance.
+            Id = Guid.NewGuid();
+        }
         
         public Author(string firstName, string lastName, DateTime birthdate)
+            : this() // Call the default constructor to initialize the Id
         {
             FirstName = firstName;
             LastName = lastName;
-            Birthdate = birthdate;
+            Birthdate = birthdate.ToUniversalTime();
         }
 
-        public int Id { get; set; }
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
 
         [Required]
         [StringLength(100)]
@@ -25,10 +34,15 @@ namespace Example.Models
         [StringLength(100)]
         [DisplayName("Last name")]
         public string LastName { get; set; } = string.Empty;
-
+        
+        private DateTime _birthdate;
         [DataType(DataType.Date)]
         [DisplayName("Birthdate")]
-        public DateTime Birthdate { get; set; }
+        public DateTime Birthdate
+        {
+            get => _birthdate;
+            set => _birthdate = value.ToUniversalTime(); // Convert to UTC
+        }
 
         public ICollection<Review> Reviews { get; set; } = new List<Review>();
     }
